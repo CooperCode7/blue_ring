04158b16d32648d021dcc4e3c8c985a828666c4c
GoldHeeler <61284229+GoldHeeler@users.noreply.github.com>
2022-11-13 05:32:19 -0600

    Remove classes for now Why They are more trouble than they are worth at the moment. The code is pretty small so the classes just cause more inconvenience when working in heroku.
    How
    - Remove the classes and revert the methods to functions

--- octopus/octopus/calibrator.py
+++ octopus/octopus/calibrator.py
@@ -12,75 +12,70 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 
-class Extract:
-    def sheet_pull(self):
-        """Pulls data from the Google Sheet into a Pandas dataframe. This will
-        subsequently be loaded into a Postgres database."""
+def sheet_pull():
+    """Pulls data from the Google Sheet into a Pandas dataframe. This will
+    subsequently be loaded into a Postgres database."""
 
-        # If modifying these scopes, delete the file token.json.
-        SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    # If modifying these scopes, delete the file token.json.
+    SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-        # The ID and range of a sample spreadsheet.
-        SPREADSHEET_ID = "1so7AoYxZ2NVG2IHdU4u8pQ2cZfEAGyj4GpqcIeR0hYg"
-        RANGE_NAME = "responses"
+    # The ID and range of a sample spreadsheet.
+    SPREADSHEET_ID = "1so7AoYxZ2NVG2IHdU4u8pQ2cZfEAGyj4GpqcIeR0hYg"
+    RANGE_NAME = "responses"
 
-        creds = None
-        # The file token.json stores the user's access and refresh tokens, and is
-        # created automatically when the authorization flow completes for the first
-        # time.
-        if os.path.exists("token.json"):
-            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    "credentials.json", SCOPES
-                )
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open("token.json", "w") as token:
-                token.write(creds.to_json())
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-        try:
-            service = build("sheets", "v4", credentials=creds)
+    try:
+        service = build("sheets", "v4", credentials=creds)
 
-            # Call the Sheets API
-            sheet = service.spreadsheets()
-            result = (
-                sheet.values()
-                .get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME)
-                .execute()
-            )
-            values = result.get("values", [])
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+        )
+        values = result.get("values", [])
 
-            if values:
-                return pd.DataFrame(values)
-            else:
-                print("No data found.")
-                return
+        if values:
+            return pd.DataFrame(values)
+        else:
+            print("No data found.")
+            return
 
-        except HttpError as err:
-            print(err)
+    except HttpError as err:
+        print(err)
 
 
-class Load:
-    def pg_conn(self):
-        """Establish the connection to the postgres database."""
+def pg_conn():
+    """Establish the connection to the postgres database."""
 
-        # Heroku stores the dialect as "postgres" but SQLAlchemy requires "postgresql"
-        conn_string = os.getenv("DATABASE_URL")
-        if conn_string and conn_string.startswith("postgres://"):
-            conn_string = conn_string.replace("postgres://", "postgresql://", 1)
+    # Heroku stores the dialect as "postgres" but SQLAlchemy requires "postgresql"
+    conn_string = os.getenv("DATABASE_URL")
+    if conn_string and conn_string.startswith("postgres://"):
+        conn_string = conn_string.replace("postgres://", "postgresql://", 1)
 
-        db = create_engine(conn_string)
-        db_conn = db.connect()
-        return psycopg2.connect(db_conn)
+    db = create_engine(conn_string)
+    db_conn = db.connect()
+    return psycopg2.connect(db_conn)
 
-    def data_push(self):
-        """Push the data from the dataframe to the postgres database."""
 
-        with self.pg_conn() as conn:
-            sql_df = Extract.sheet_pull()
-            sql_df.to_sql("tracker", con=conn, if_exists="replace", index=False)
+def data_push(self):
+    """Push the data from the dataframe to the postgres database."""
+
+    with pg_conn() as conn:
+        sql_df = sheet_pull()
+        sql_df.to_sql("tracker", con=conn, if_exists="replace", index=False)
