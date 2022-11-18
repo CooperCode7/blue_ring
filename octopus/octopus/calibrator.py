f09dd50c0588014723cd19b9afd1e2314cfc78be
GoldHeeler <61284229+GoldHeeler@users.noreply.github.com>
2022-11-17 22:44:47 -0600

    Add spaces between comments for readability

--- octopus/octopus/calibrator.py
+++ octopus/octopus/calibrator.py
@@ -25,11 +25,13 @@ def sheet_pull():
     RANGE_NAME = "responses"
 
     creds = None
+
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -37,6 +39,7 @@ def sheet_pull():
         else:
             flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
+
         # Save the credentials for the next run
         with open("token.json", "w") as token:
             token.write(creds.to_json())
@@ -53,6 +56,7 @@ def sheet_pull():
 
         if values:
             df = pd.DataFrame(values)
+
             # Fix the dataframe so the first row is treated as the header row
             df.columns = df.iloc[0]
             df = df[1:]
