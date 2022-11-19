8a2ba2fc5e000ec229caa8be22a6f6350b9dfc8c
NuclearOctopus <61284229+NuclearOctopus@users.noreply.github.com>
2022-11-18 21:44:59 -0600

    Change POC name for app folder Why Calling the app folder OG makes more sense since it contains a modified version of the original code for this project.

--- og/og/tests/test_calibrator.py
+++ og/og/tests/test_calibrator.py
@@ -1,4 +1,4 @@
-# Append octopus folder to path so the functions to test can be imported
+# Append app folder to path so the functions to test can be imported
 import os, sys
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
