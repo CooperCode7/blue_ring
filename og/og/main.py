2a19d3cfd6e2dd8e0539897510362c5c2ddadc8c
NuclearOctopus <61284229+NuclearOctopus@users.noreply.github.com>
2022-11-19 18:12:57 -0600

    Add comments and expander for dataframe

--- og/og/main.py
+++ og/og/main.py
@@ -1,8 +1,19 @@
 import os
+import pandas as pd
 import calibrator
 
+# Wrap in this if statement to prevent accidental runs on import
 if __name__ == "__main__":
-    predicted_df, accuracy, cv_score = calibrator.reconnect(os.getenv("CALIBRATOR_LABEL_NAME"))
+
+    # Want to see all the columns in the dataframe
+    pd.set_option("display.max_columns", None)
+
+    # Pull in the necessary objects
+    predicted_df, accuracy, cv_score = calibrator.reconnect(
+        os.getenv("CALIBRATOR_LABEL_NAME")
+    )
+
+    # Print out the dataframe as well as the scores
     print(predicted_df.tail(10))
     print(f"Accuracy: {accuracy}")
     print(f"CV Score: {cv_score}")
