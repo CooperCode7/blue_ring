6e04a4c2d2cb0cdf2daa5bf97d7505a56633841f
NuclearOctopus <61284229+NuclearOctopus@users.noreply.github.com>
2022-11-20 20:13:53 -0600

    Refine the "main" output Why In addition to the feature importance viz, I also want the dataframe output easier to interpret.
    How
    - Change the dataframe so it only outputs the timestamp, event, and
    predicted event fields for the past 30 days
    - Add a call to the prioritize function to get the feature viz

--- og/og/main.py
+++ og/og/main.py
@@ -5,15 +5,15 @@ import calibrator
 # Wrap in this if statement to prevent accidental runs on import
 if __name__ == "__main__":
 
-    # Want to see all the columns in the dataframe
-    pd.set_option("display.max_columns", None)
+    label_name = os.getenv("CALIBRATOR_LABEL_NAME")
 
     # Pull in the necessary objects
-    predicted_df, accuracy, cv_score = calibrator.reconnect(
-        os.getenv("CALIBRATOR_LABEL_NAME")
-    )
+    predicted_df, accuracy, cv_score = calibrator.reconnect(label_name)
 
-    # Print out the dataframe as well as the scores
-    print(predicted_df.tail(10))
+    # # Print out the prediction vs. actual for the last 30 days as well as the accuracy scores
+    print(predicted_df[['Timestamp','event', 'predicted_event']].tail(30))
     print(f"Accuracy: {accuracy}")
     print(f"CV Score: {cv_score}")
+
+    # Create the feature importance visualization
+    calibrator.prioritize(label_name)
