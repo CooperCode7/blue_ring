6bef690540a4ababab57a7da7902d4b056e5fc7e
NuclearOctopus <61284229+NuclearOctopus@users.noreply.github.com>
2022-12-03 14:35:54 -0600

    Move output tables to calibrator file

--- og/og/main.py
+++ og/og/main.py
@@ -2,29 +2,6 @@ import os
 import pandas as pd
 import calibrator
 
-
-def get_prediction_table(label_name):
-    """Output a table with the last 90 days' worth of predictions."""
-
-    # Pull in the necessary objects
-    predicted_df, accuracy, cv_score = calibrator.reconnect(label_name)
-
-    # Set the DataFrame to show all rows
-    pd.set_option("display.max_rows", None)
-
-    # # Print out the prediction vs. actual for the last 90 days as well as the accuracy scores
-    print(predicted_df[["Timestamp", "event", "predicted_event"]].tail(90))
-    print("Accuracy: " + "{:.0%}".format(accuracy))
-    print(f"CV Score: " + "{:.0%}".format(cv_score))
-
-
-def get_feature_importance(label_name):
-    """Create a visualization of feature importances tied to the predictions."""
-
-    # Create the feature importance visualization
-    calibrator.prioritize(label_name)
-
-
 if __name__ == "__main__":
 
     label_name = os.getenv("CALIBRATOR_LABEL_NAME")
@@ -32,8 +9,8 @@ if __name__ == "__main__":
     user_input = input("What data would you like to see? ").lower()
 
     if user_input == 'prediction table':
-        get_prediction_table(label_name)
+        calibrator.get_prediction_table(label_name)
     elif user_input == 'feature importance':
-        get_feature_importance(label_name)
+        calibrator.get_feature_importance(label_name)
     else:
         print("Incorrect argument supplied.")
