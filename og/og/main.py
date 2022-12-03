d534888a906725dca3a2126d7f739380bf54c7b7
NuclearOctopus <61284229+NuclearOctopus@users.noreply.github.com>
2022-12-03 13:45:35 -0600

    Add user input option for choosing function Why It makes more sense to allow the user to choose the function they want to run rather than running all of them. This will be especially useful as more output functions are added to the main script.
    How
    - Make functions for the prediction table and feature importance viz
    - Clean up the formatting on the accuracy and cv scores
    - Add a user input section that runs the correct function if the user
    enters the right argument

--- og/og/main.py
+++ og/og/main.py
@@ -1,11 +1,11 @@
 import os
+import sys
 import pandas as pd
 import calibrator
 
-# Wrap in this if statement to prevent accidental runs on import
-if __name__ == "__main__":
 
-    label_name = os.getenv("CALIBRATOR_LABEL_NAME")
+def get_prediction_table(label_name):
+    """Output a table with the last 90 days' worth of predictions."""
 
     # Pull in the necessary objects
     predicted_df, accuracy, cv_score = calibrator.reconnect(label_name)
@@ -15,8 +15,26 @@ if __name__ == "__main__":
 
     # # Print out the prediction vs. actual for the last 90 days as well as the accuracy scores
     print(predicted_df[["Timestamp", "event", "predicted_event"]].tail(90))
-    print(f"Accuracy: {accuracy}")
-    print(f"CV Score: {cv_score}")
+    print("Accuracy: " + "{:.0%}".format(accuracy))
+    print(f"CV Score: " + "{:.0%}".format(cv_score))
+
+
+def get_feature_importance(label_name):
+    """Create a visualization of feature importances tied to the predictions."""
 
     # Create the feature importance visualization
     calibrator.prioritize(label_name)
+
+
+if __name__ == "__main__":
+
+    label_name = os.getenv("CALIBRATOR_LABEL_NAME")
+
+    user_input = input("What data would you like to see? ").lower()
+
+    if user_input == 'prediction table':
+        get_prediction_table(label_name)
+    elif user_input == 'feature importance':
+        get_feature_importance(label_name)
+    else:
+        print("Incorrect argument supplied.")
