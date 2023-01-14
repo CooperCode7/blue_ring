15414b1db6e2958db3882f58165effad10a189ce
NuclearOctopus <61284229+NuclearOctopus@users.noreply.github.com>
2023-01-14 09:51:07 -0600

    Use variable in place of hardcoded name

--- og/og/calibrator.py
+++ og/og/calibrator.py
@@ -255,7 +255,7 @@ def get_prediction_table(label_name):
     pd.set_option("display.max_rows", None)
 
     # # Print out the prediction vs. actual for the last 90 days as well as the accuracy scores
-    print(predicted_df[["Timestamp", "event", "predicted_event"]].tail(90))
+    print(predicted_df[["Timestamp", label_name, "predicted_event"]].tail(90))
     print("Accuracy: " + "{:.0%}".format(accuracy))
     print(f"CV Score: " + "{:.0%}".format(cv_score))
 
