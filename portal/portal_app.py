525ded79b76a5aa99441af9b9c9e60680c68de4c
Mr. Snrub <steamedhams722@protonmail.com>
2024-03-03 10:35:03 -0600

    Add Flask app for recording survey answers
    Why
    This is the initial version of the app that
    will be used to record answers on a mobile friendly website.
    It replaces the Google Form set-up
    
    How
    Create a Flask app that has temp storage for questions and
    allows users to submit answers. Note that this is still a work
    in progress and may not work in its current state.
    Note that this is still a work in progress

---
+++ portal/portal_app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Temporary storage for survey questions
+questions = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create_survey', methods=['GET', 'POST'])
+def create_survey():
+    if request.method == 'POST':
+        question = request.form['question']
+        questions.append({'question': question})
+        return redirect(url_for('create_survey'))
+    return render_template('create_survey.html', questions=questions)
+
+@app.route('/take_survey', methods=['GET', 'POST'])
+def take_survey():
+    if request.method == 'POST':
+        # Process submitted answers here
+        answers = request.form
+        # For demo, just print answers
+        print(answers)
+        return redirect(url_for('index'))
+    return render_template('take_survey.html', questions=questions)
+
+if __name__ == '__main__':
+    app.run(debug=True)
